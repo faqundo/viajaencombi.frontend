822c6efa95f88bd13c7e4142e3bd7acf75d95201
FaQcundo <procesando.energias@gmail.com>
2020-05-16 03:08:59 -0300

    logo

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,13 +5,18 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import logo from "../logo.jpg";
 
 function NavigationBar() {
 
     return (
 
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Combi App</Navbar.Brand>
+        <Navbar bg="dark" expand="lg">
+            
+            <Navbar.Brand href="#home">
+                <img src={logo}> </img>
+                <Navbar.Text>Combi App</Navbar.Text>
+            </Navbar.Brand>
             
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             
@@ -27,10 +32,12 @@ function NavigationBar() {
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
+
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-success">Search</Button>
                 </Form>
+
             </Navbar.Collapse>
         </Navbar>
 
