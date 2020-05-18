5021762af441101cfb8a4ca126b0da3e3d9cd956
FaQcundo <procesando.energias@gmail.com>
2020-05-17 22:28:01 -0300

    icon favorito

--- src/components/ListadoViajes.js
+++ src/components/ListadoViajes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row , Col , Card } from "react-bootstrap";
+import iconoNoFavorito from '../nofavorito.svg'
 
 const ListadoViajes = () => {
 
@@ -12,10 +13,15 @@ const ListadoViajes = () => {
                 <Card>
 
                     <Card.Body>
+
+                        <a className="nav-link p-0 text-right" href="#">
+                            <img style={{height : "1rem" }}src={iconoNoFavorito}></img>
+                        </a>
+
                         <Card.Title>
                             Viaje 1
                         </Card.Title>
-                        <img src="" className="card-img-top">
+                        <img src="https://d2idryjws00qar.cloudfront.net/aiv/gra/image/home/carrousel-home-villa-gesell-bs-as.png" className="card-img-top">
 
                         </img>
                     </Card.Body>
