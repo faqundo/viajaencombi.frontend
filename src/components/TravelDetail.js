10509f33a8317e7b20d25fc24ba9210c3ecb6c3f
FaQcundo <procesando.energias@gmail.com>
2020-05-20 20:56:49 -0300

    TravelDetail.js useParams nuevo Hooks

--- src/components/TravelDetail.js
+++ src/components/TravelDetail.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
+import {Row,Col} from 'react-bootstrap';
+import { useParams } from "react-router-dom";
 
 export default () => {
 
+    let {id} = useParams();
+
+    let [destino ,setDestino ] = useState(null);
+
+    useEffect (
+        ()=>{
+            fetch('http://localhost:8888/destinos/' +id).then(
+                response => response.json()
+            ).then (data => {
+                setDestino(data);
+                console.log(data);
+            })
+        },[]
+    )
+
     return (
-        <div>
-            <p>Detalle de viaje seleccionado</p>
-        </div>
+        
+        destino && 
+        <Row className="d-flex justify-content-center">
+            <Col md={4} className="d-flex justify-content-center">
+                <img src="http://localhost:8888/images/ezeiza.jpg" className="img-fluid"/>
+            </Col>
+
+            <Col md={4}>
+                <h2>Nombre Destino</h2>
+                <h3>Precio</h3>
+            </Col>
+        </Row>
+
     );
 }
 
